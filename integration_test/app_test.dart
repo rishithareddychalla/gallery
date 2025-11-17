28ef5ebed9b99d3634d5dfe7851dc4c2da2f0aba
Rishitha Reddy <rishithareddychalla@gmail.com>
2025-11-17 20:21:34 +0530

    logo

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
+
 import 'package:gallery/main.dart' as app;
 
 void main() {
+  var IntegrationTestWidgetsFlutterBinding;
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Navigate to image editor and take screenshot', (WidgetTester tester) async {
